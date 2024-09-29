d3ff47782f0c0402289b3ebe21a359b473efb58a
BarnabasKalasz <barnabaspeter.kalasz@gmail.com>
2024-09-29 20:19:36 +0200

    fixed docker compose yaml

--- app/lib/db/mongodb.ts
+++ app/lib/db/mongodb.ts
@@ -1,19 +1,18 @@
 import { MongoClient, Db } from 'mongodb';
 
-const uri = process.env.MONGODB_URI; // MongoDB URI from your .env file
+const uri = process.env.MONGODB_URI;
 let db: Db | undefined;
 
-// Create a function to connect to the database
 export async function connectToDatabase() {
   if (!uri) {
     throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
   }
 
-  if (db) return db; // Return existing db connection if already connected
+  if (db) return db;
 
   const client = new MongoClient(uri);
 
   await client.connect();
-  db = client.db('task_manager'); // Create or switch to task_manager database
+  db = client.db('task_manager');
   return db;
 }
